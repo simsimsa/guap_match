c5b0dd961d8d51a70d146eaf5e952f398ceaf65f
Ksusha <samandarovak@mail.ru>
2025-02-21 14:31:56 +0300

    fix Gallery

--- guapMatch/src/pages/Profile/User.tsx
+++ guapMatch/src/pages/Profile/User.tsx
@@ -1,5 +1,14 @@
 import styles from "./User.module.css";
 
+const testImg = [
+    "./test1.jpg",
+    "./roma.jpg",
+    "./test3.jpg",
+    "./test2.jpg",
+    "./test4.jpg",
+    "test5.jpg",
+];
+
 export function User() {
     return (
         <div className={styles["user_layout"]}>
@@ -48,12 +57,18 @@ export function User() {
                     Здесь будет о себе
                 </div>
             </div>
-            <div className={styles['user_body']}>
-                <div className={styles['user_gallery']}>
+            <div className={styles["user_body"]}>
+                <div className={styles["user_gallery"]}>
                     <div>Gallery</div>
-                    <div>Тут будет ссылка</div>
+                    <div className={styles["all_photos"]}>See all</div>
                 </div>
-                <div>Тут будут все загруженные фотографии</div>
+                <div className={styles["gallery_img"]}>
+                    {testImg.map((photo) => {
+                        return (
+                            <img src={photo} className={styles["photo_user"]} />
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
